2658899525edd2a3fea4081ecfaf896eb6e09c4e
somemorewater <oboyiloejeh@gmail.com>
2026-01-10 16:39:50 +0100

    feat: implement WebSocket event handlers for user connections and room management

--- server.js
+++ server.js
@@ -16,5 +16,32 @@ const server = app.listen(PORT, () => {
 
 const io = socketIo(server);
 io.on("connection", socket => {
+    console.log("A user connected", socket.id);
     
+
+    //Join a room
+    socket.on("join", roomId => {
+        socket.join(roomId);
+        console.log(`${socket.id} joined room ${roomId}`);
+    });
+
+    //Forward offer to other peer(S)
+    socket.on("offer", ({ roomId, offer }) => {
+        socket.to(roomId).emit("offer", { offer, from: socket.id });
+    });
+
+    //Forward answer to other peer(s)
+    socket.on("answer", ({ roomId, answer }) => {
+        socket.to(roomId).emit("answer", { answer, from: socket.id });
+    });
+
+    //Forward ICE candidates
+    socket.on("ice", ({ roomId, candidate }) => {
+        socket.to(roomId).emit("ice", { candidate, from: socket.id });
+    });
+
+    //Disconnect
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
 })
